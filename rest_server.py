227f1751ca991ae69b5fd98e43a88ab6da3e14c8
Linda Grealish <lindagrealish@hotmail.com>
2024-08-04 15:37:05 +0100

    Adding CRUD code to rest_server.py

--- rest_server.py
+++ rest_server.py
@@ -7,5 +7,55 @@ app = Flask(__name__, static_url_path='',static_folder='staticpages')
 def index():
     return "Test"
 
+# Get All
+@app.route('/players', methods=['GET'])
+def getAll():
+    return jsonify(players)
+
+# Find by Name
+@app.route('/players/<int:id>', methods=['GET'])
+def findbyid():
+    return jsonify(players)
+
+# Create
+@app.route('/players', methods=['POST'])
+def create():
+        # read json from the body
+        jsonstring = request.json
+        player = {}
+
+        if "name" not in jsonstring:
+                abort(402)
+        player["name"] = jsonstring["name"]
+        if "club" not in jsonstring:
+                abort(402)
+        player["club"] = jsonstring["club"]
+        if "age" not in jsonstring:
+                abort(402)
+        player["age"] = jsonstring["age"]
+        if "position" not in jsonstring:
+                abort(402)
+        player["position"] = jsonstring["position"]
+        
+        return jsonify(playerDAO.create(player))
+
+# Update
+@app.route('/players', methods=['PUT'])
+def update():
+        jsonstring = request.json
+        if "name" in jsonstring:
+                player["name"] = jsonstring["name"]
+        if "club" in jsonstring:
+                player["club"] = jsonstring["club"]
+        if "age" in jsonstring:
+                player["age"] = jsonstring["age"]
+        if "position" in jsonstring:
+                player["position"] = jsonstring["position"]
+    
+        return jsonify(playerDAO.update(id, player))
+       
+
+
+   
 if __name__ == '__main__':
     app.run(debug=True)
